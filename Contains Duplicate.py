806cc26cd5f0b845f7b9f80161c53f27c5880011
Ram Rohit Gannavarapu <ganna.ramu@gmail.com>
2020-06-26 16:39:25 -0600

    Array

---
+++ Contains Duplicate.py
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        for(int i=0;i<nums.size();i++){
+            counts[nums[i]]++;
+        }
+        for (auto count : counts)
+             if (count.second>1) return true;
+         return false;
+    }
+};
